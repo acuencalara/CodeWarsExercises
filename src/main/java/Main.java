9f563687c6e473dd03241d80e5c1d15ea74b85af
acuencalara <acuencalara@gmail.com>
2022-08-29 15:50:05 +0200

    Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,11 @@ public class Main {
     }
 
     public static void callMethod(){
+        System.out.println("Primer ejercicio: \n");
         JadenCase phrase = new JadenCase();
         JadenCase.toJadenCase("Most Trees Are Blue");
 
+        System.out.println("\nSegundo ejercicio: \n");
         DisemvowelTrolls example= new DisemvowelTrolls();
         DisemvowelTrolls.disemvowel("This website is for losers LOL!");
 
